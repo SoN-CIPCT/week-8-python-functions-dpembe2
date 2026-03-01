8d7619ed1b1a4d77cb6b6a80da144733cf99a5c2
dpembe2 <dpembe2@uw.edu>
2026-03-01 01:38:28 -0800

    Update homework8_Deborah_Pemberton.py

--- homework8_Deborah_Pemberton.py
+++ homework8_Deborah_Pemberton.py
@@ -1,1 +1,7 @@
-
+def create_sandwich(*ingredients):
+    print("You've ordered a sandwich with the following ingredients:")
+    for ingredient in ingredients:
+        print(f"- {ingredient}")
+create_sandwich("Lettuce", "Tomato", "Turkey", "Cheese")
+print()  # Just for spacing
+create_sandwich("Ham", "Mustard")
